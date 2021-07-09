7c49b5355d113d9014f2f8f7bcd0cb544ef75c07
Giulio <62805702+knightrider909@users.noreply.github.com>
2021-07-09 09:39:00 +0100

    email valid attempt

--- dist/emailValid.js
+++ dist/emailValid.js
@@ -14,15 +14,54 @@ function manage(emailForm) {
 
 // email address error pop up message
 
-function emailAddress() {
-	var input = document.getElementById("emailAddress");
-	input.oninvalid = function (event) {
-		event.target.setCustomValidity(
-			"Email address should be entered in the following format: email@tester.com"
-		);
-	};
-}
-emailAddress();
+// const email = document.querySelector("#emailForm");
+// const submit = document.querySelector("#submit");
+
+// submit.addEventListener("click", () => {
+// 	if (email.validity.typeMismatch) {
+// 		email.setCustomValidity(
+// 			"Email address should be entered in the following format: email@tester.com"
+// 		);
+// 	} else {
+// 		email.setCustomValidity("");
+// 	}
+// });
+
+// let checkEmail = document.querySelector("#submit");
+// checkEmail.addEventListener("click", ValidateEmail);
+
+// function emailAddress() {
+// 	var input = document.getElementById("emailAddress");
+// 	input.oninvalid = function (event) {
+// 		event.target.setCustomValidity(
+// 			"Email address should be entered in the following format: email@tester.com"
+// 		);
+// 	};
+// }
+
+// function ValidateEmail() {
+// 	let inputText = document.querySelector("#emailAddress");
+// 	inputText.oninvalid = function (e) {
+// 		if (!e.target.validity.valid) {
+// 			e.target.setCustomValidity(
+// 				"Email address should be entered in the following format: email@tester.com"
+// 			);
+// 		}
+// 	};
+
+// 	var mailformat =
+// 		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// 	if (inputText.value.match(mailformat)) {
+// 		return null;
+// 	} else {
+// 		inputText.oninvalid = function (event) {
+// 			event.target.setCustomValidity(
+// 				"Email address should be entered in the following format: email@tester.com"
+// 			);
+// 		};
+
+// 	}
+// }
 
 // function checker() {
 // 	let emailFrom = document.getElementById("emailForm");
